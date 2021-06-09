2bfe0344bcaf49531da399968e284f88aa027d9e
viisheshjain05 <viisheshjain05@gmail.com>
2021-06-09 17:04:03 +0530

    Added npm package as local package using git clone

--- section5 EXPRESS/expressApp.js
+++ section5 EXPRESS/expressApp.js
@@ -3,38 +3,47 @@
 // * FileName            -   expressApp
 
 // * Contain consepts                   - [middleware, ];
-// * Contain Function - app[express]    - [use(), ];
+// * Contain Function - app[express]    - [use(), res.send() ];
 
 // INDEX
 // 1) middleware
-// 2)
+// 2) Sending Data
 // 3)
 // 4)
 // 5)
 
 //* Node Functions
-const http = require("http");
+// const http = require("http");
 
 // Libraries
 const express = require("express");
+// const classnames = require("classname");
+const { classnames } = require("../utils/venderExport");
 
 const ExpressApp = (props) => {
   // 1) middleware EXPRESS INTI //* Init and Create an Express application
   const app = express();
+  console.log(classnames("this is from Classname"));
 
   // 1) middleware //* Use allow to create a middleware where "next" Allows to get to another middleware only when it is passed.
+
+  // 1) middleware //* We always travel top to bottom by calling next() in middleware to middleware
   app.use((req, res, next) => {
     console.log("Inside Middleware");
+
+    // 1) middleware //* allow the request to continue to next middleware inline as exit from this.
     next();
   });
 
   app.use((req, res, next) => {
     console.log("Inside Another Middleware");
+    // 2) Sending Data //* Send is express function allow to send data without any tention to setHeader() || end() || write() the res function we can send files chunks any type of data
+
+    res.send("<h1>Hii from EXPRESS JS </h1>");
   });
 
-  const server = http.createServer(app);
-
-  server.listen("3000", "localhost");
+  // 1) middleware //* We can also use listen on app or express as behind the sceen express will use createServer and pass the data and listen function to it so it will work fine
+  app.listen("3000", "localhost");
 };
 
 //? MIDDLEWARE - it allows to hook new function inside the bigger function to bind and send responce together
---
+++ utils/venderExport.js
@@ -0,0 +1,6 @@
+// use This file to export all the packages defined below
+
+"use strict";
+
+module.exports.classnames = require("./vender/classnames/index.js");
+module.exports.bodyParser = require("./vender/body-parser/index.js");
